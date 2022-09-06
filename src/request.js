ebb20ef94b7594a95b7cfc1906ca11829911aa83
Burcu Saglam <saglamburcu96@gmail.com>
2022-09-06 18:26:32 +0300

    Created request class

---
+++ src/request.js
@@ -0,0 +1,44 @@
+export class Request {
+  constructor(url) {
+    this.url = url;
+  }
+
+  async get() {
+    const response = await fetch(this.url);
+    const data = response.json();
+
+    return data;
+  }
+
+  async post(data) {
+    const response = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    });
+
+    const responseData = response.json();
+
+    return responseData;
+  }
+
+  async put(id, data) {
+    const response = await fetch((`${this.url}/${id}`), {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    });
+
+    const responseData = response.json();
+
+    return responseData;
+  }
+
+  async delete(id) {
+    const response = await fetch((`${this.url}/${id}`), {
+      method: "DELETE"
+    })
+
+    return "Veri silme işlemi başarılı"
+  }
+}
